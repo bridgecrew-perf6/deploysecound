e17ed2d47f6a660c0ee7f5918ae5ed7e0d336cf4
deepakone95 <deepakone95@gmail.com>
2022-01-16 18:03:11 +0530

    third

--- newpro/settings.py
+++ newpro/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'o!1rd($jskwpaev9@h9up^-jtqdm2s7jhlfmdkpmok*b0il=&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1'
-    'django-secound.herokuapp.com']
+ALLOWED_HOSTS = ['django-secound.herokuapp.com']
 
 
 # Application definition
